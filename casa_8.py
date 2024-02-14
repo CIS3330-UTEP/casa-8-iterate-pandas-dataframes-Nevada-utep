eff2f95c13a67be950b69b78bb4096886ca0b136
jkpohan <jkpohan@miners.utep.edu>
2024-02-14 08:17:58 -0700

    casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+df = pd.read_csv('big-mac-full-index.csv')
+
+# iterrows method
+
+for i,r in df.iterrows():
+    print(r['name'], r['local_price'])
+
+
+# apply method
+
+print(df.apply(lambda row: row['local_price'], axis =1))
